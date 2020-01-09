c1e47b944794b7997dc7c71ea27c964a903bf0c0
Ian Hutson <ih00209@georgiasouthern.edu>
2020-01-09 18:34:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  monopoly = {:railroads => {:rent_in_dollars => 5, :pieces => 4}}
+  monopoly = {:railroads => {:rent_in_dollars => 4, :pieces => 4}}
 end
 
 def monopoly_with_fourth_tier
