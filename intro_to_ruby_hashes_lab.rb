f7fd9e620aab2d84926aaad82fdde644f5b7e9cd
Ian Hutson <ih00209@georgiasouthern.edu>
2020-01-09 18:30:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def base_hash
-  monopoly = {:railroads => {:pieces => ""}}
+  monopoly = {:railroads => {}}
 end
 
 def monopoly_with_second_tier
-  monopoly = {:railroads => {}}
+  monopoly = {:railroads => {:pieces => ""}}
 end
 
 def monopoly_with_third_tier
